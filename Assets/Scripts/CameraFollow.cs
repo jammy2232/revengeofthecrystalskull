49c7303f0f2bd0f167016fe8f63d478442182448
James McAdam <j.g.mcadam@gmail.com>
2017-03-06 17:37:55 +0000

    Created a test ship to fly around the level based on Same Prefab. - It looks bad but should let you use wasd to move around.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	TESTPLAYER temp;
+	Vector3 cameraPos;
+
+	// Use this for initialization
+	void Start () {
+
+		temp = FindObjectOfType<TESTPLAYER> ();
+		cameraPos = temp.transform.position - transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = temp.transform.position - cameraPos;
+		
+	}
+}

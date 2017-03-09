dcd829a9c5d7acc7b31f4247e993fb21e7e3cd25
niklas-evertsson <niklas.evertsson@gmail.com>
2017-03-09 17:00:56 +0000

    Added AirConsole plugin and scripts.

---
+++ Assets/Scripts/AirConsoleLogic.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using NDream.AirConsole;
+using Newtonsoft.Json.Linq;
+
+public class AirConsoleLogic : MonoBehaviour {
+
+	[HideInInspector]
+	public bool thruster1;
+	[HideInInspector]
+	public bool thruster2;
+	[HideInInspector]
+	public bool thruster3;
+	[HideInInspector]
+	public bool thruster4;
+
+	public Text uiText;
+
+	public int numberOfPlayers = 4;
+
+#if !DISABLE_AIRCONSOLE 
+
+	void Awake () {
+		AirConsole.instance.onMessage += OnMessage;
+		AirConsole.instance.onConnect += OnConnect;
+		AirConsole.instance.onDisconnect += OnDisconnect;
+	}
+
+	/// <summary>
+	/// We start the game if 4 players are connected and the game is not already running (activePlayers == null).
+	/// 
+	/// NOTE: We store the controller device_ids of the active players. We do not hardcode player device_ids 1 and 2,
+	///       because the two controllers that are connected can have other device_ids e.g. 3 and 7.
+	///       For more information read: http://developers.airconsole.com/#/guides/device_ids_and_states
+	/// 
+	/// </summary>
+	/// <param name="device_id">The device_id that connected</param>
+	void OnConnect (int device_id) {
+		if (AirConsole.instance.GetActivePlayerDeviceIds.Count == 0) {
+			if (AirConsole.instance.GetControllerDeviceIds ().Count >= numberOfPlayers) {
+				StartGame ();
+			} else {
+				uiText.text = "NEED MORE PLAYERS";
+			}
+		}
+	}
+
+	/// <summary>
+	/// If the game is running and one of the active players leaves, we reset the game.
+	/// </summary>
+	/// <param name="device_id">The device_id that has left.</param>
+	void OnDisconnect (int device_id) {
+		int active_player = AirConsole.instance.ConvertDeviceIdToPlayerNumber (device_id);
+		if (active_player != -1) {
+			if (AirConsole.instance.GetControllerDeviceIds ().Count >= numberOfPlayers) {
+				StartGame ();
+			} else {
+				AirConsole.instance.SetActivePlayers (0);
+				uiText.text = "PLAYER LEFT - NEED MORE PLAYERS";
+			}
+		}
+	}
+
+	/// <summary>
+	/// We check which one of the active players wants to move.
+	/// </summary>
+	/// <param name="from">From.</param>
+	/// <param name="data">Data.</param>
+	void OnMessage (int device_id, JToken data) {
+		int active_player = AirConsole.instance.ConvertDeviceIdToPlayerNumber (device_id);
+		if (active_player != -1) {
+			if (active_player == 0) {
+				thruster1 = (bool)data["move"];
+			}
+			if (active_player == 1) {
+				thruster2 = (bool)data["move"];
+			}
+			if (active_player == 2) {
+				thruster3 = (bool)data["move"];
+			}
+			if (active_player == 3) {
+				thruster4 = (bool)data["move"];
+			}
+		}
+	}
+
+	void StartGame () {
+		AirConsole.instance.SetActivePlayers (numberOfPlayers);
+	}
+
+	void OnDestroy () {
+
+		// unregister airconsole events on scene change
+		if (AirConsole.instance != null) {
+			AirConsole.instance.onMessage -= OnMessage;
+		}
+	}
+#endif
+}
